8e1062e1ccd3b6a694aded010c71a8feafe01f60
benedictodzibdzib@gmail.com <benedictodzibdzib@gmail.com>
2025-09-22 11:06:04 -0600

    conexion a postgres

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cfcp',
+        'USER': 'postgres',
+        'PASSWORD': 'Cfcp2025*$/',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
